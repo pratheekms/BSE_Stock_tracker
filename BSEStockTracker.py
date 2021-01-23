f30dbffd4cbd905b02b668f23da6db433422eb0c
pratheek <ms.pratheek1@gmail.com>
2021-01-23 23:25:21 +0530

    google search working

--- BSEStockTracker.py
+++ BSEStockTracker.py
@@ -1,23 +1,36 @@
 import time
+
 start = time.time()
 import bs4, requests
 import openpyxl
 import random
 import logging
 import urllib3
-#from google import search
+# from google import search
 
 from googlesearch import search
 
-
 print("hello world")
 
-StockName='TCS'
-BSEUrl=search(query,        # The query you want to run
-                tld = 'com',  # The top level domain
-                lang = 'en',  # The language
-                num = 10,     # Number of results per page
-                start = 0,    # First result to retrieve
-                stop = None,  # Last result to retrieve
-                pause = 2.0,  # Lapse between HTTP requests
-               ):
+StockName = ['TCS', 'WIPRO', 'INFOSYS']
+SearchQuery=[]
+# Query generator for google search
+
+for i in StockName:
+    SearchQuery.append(i + " share BSE")
+
+for eachStock in SearchQuery:
+
+query = 'TCS'
+for i in search(query,  # The query you want to run
+                tld='com',  # The top level domain
+                lang='en',  # The language
+                num=10,  # Number of results per page
+                start=0,  # First result to retrieve
+                stop=1,  # Last result to retrieve
+                pause=2.0,  # Lapse between HTTP requests
+                ):
+    BSEUrl = i
+    print("BSE Url-->" + BSEUrl)
+# print("url"+BSEUrl)
+print(type(BSEUrl))
